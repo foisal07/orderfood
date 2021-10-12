7e7981ca71174e7f2e886c88061054fcb91fee31
Foisal <workkaj01@gmail.com>
2021-10-12 14:38:18 +0600

    Header restyled

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,13 @@ import React from "react";
 import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import heroImage from "../../assets/heroimage.png";
+import logo from "../../assets/reactfood.png";
 
 export default function Header() {
   return (
     <>
       <header className={classes.header}>
-        <h1>OrderFood</h1>
+        <img src={logo} alt="Foods" />
         <HeaderCartButton />
       </header>
       <div className={classes.container}>
