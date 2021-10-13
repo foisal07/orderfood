52e0a8a8f8ad6d84568c2581104afd3fbae39aa8
Foisal <workkaj01@gmail.com>
2021-10-13 13:40:43 +0600

    Create Cart components and render it as modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import AvailableMeals from "./Meals/AvailableMeals";
+import Modal from "./UI/Modal";
 
 function App() {
   return (
     <div className="App">
+      <Modal>
+        <Cart />
+      </Modal>
       <Header />
       <AvailableMeals />
     </div>
---
+++ src/UI/Modal.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+export default function Modal({ children }) {
+  const Backdrop = () => {
+    return <div className={classes.backdrop} />;
+  };
+
+  const ModalContent = ({ children }) => {
+    return (
+      <div className={classes.modal}>
+        <div className={classes.content}>{children}</div>
+      </div>
+    );
+  };
+
+  const portalElement = document.getElementById("overlay");
+
+  return (
+    <>
+      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(<ModalContent>{children}</ModalContent>, portalElement)}
+    </>
+  );
+}
